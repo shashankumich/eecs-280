3e3b1e9a054b830057ff75c2bb5a3e6dd8d1d72a
Shashank Verma <shaaero@umich.edu>
2025-01-12 16:29:28 -0500

    lab1 commits

---
+++ lab/lab1/lab1.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+
+int main() {
+    // print a greeting
+    cout <<"Hello World!" << endl;
+
+    // Declare some variables
+    int x = 10+5;
+    int y =0;
+    y = x;
+    x = 20;
+
+    // Print out the results
+    cout <<"x = " <<x << ", y = " <<y << endl;
+}
